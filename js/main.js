e53fdc9928629aab49fa2ad432f69b07075836eb
Timmy <timmyjames95@gmail.com>
2018-09-22 16:51:17 +1200

    Test

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+    $('body').removeClass('fade-out');
+});
+
+$( document ).ready(function() {
+    var listOfClasses = ["color-scheme-1","color-scheme-2","color-scheme-3","color-scheme-4","color-scheme-5"];
+    var randomNum = Math.floor(Math.random() * listOfClasses.length); 
+    $("html").addClass(listOfClasses[randomNum]);
+});
+
+
+
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 950) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 900); 
+        return false; 
+    }); 
+});
+
+
+function mouseIn() {
+  $('.img').addClass('show');
+}
+
+function mouseOut() {
+  $('.img').removeClass('show');
+}
+
+$('.hover-me').hover(mouseIn, mouseOut);
